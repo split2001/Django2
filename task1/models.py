6da05f91334dec1f4377e4dea0daeafaca3495bb
Davlatov Rustam <davlatov_rm@mail.ru>
2024-11-21 14:49:29 +0300

    Django2

--- task1/models.py
+++ task1/models.py
@@ -9,6 +9,9 @@ class Buyer(models.Model):
     balance = models.DecimalField(max_digits=100, decimal_places=3)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class Game(models.Model):
     title = models.CharField(max_length=100)
@@ -22,4 +25,15 @@ class Game(models.Model):
     age_limited = models.BooleanField(default=False)
 
     buyer = models.ManyToManyField(Buyer)  # связь с моделью Buyer
+
+
+Buyer.objects.create(name='otshelnik', balance=725, age=17)
+Game.objects.create(title='GTA VI', cost=35, size=12.2, description='finally!', age_limited=True)
+Game.objects.create(title='Tanki', cost=20, size=7.4, description='tank man!', age_limited=False)
+Game.objects.create(title='CS GO II', cost=15, size=3.8, description='strike!', age_limited=False)
+Game.objects.filter(title='CS GO II').update(age_limited=True)
+
+
+
+
 
